33e7a31d13d63e479ad4902e2aec0b7a5a302307
sp.arc <pcpark98@naver.com>
2023-10-23 16:50:18 +0900

    chore: 프로젝트 세팅

---
+++ src/main/java/com/capstonedesign/inhalife/InhaLifeApplication.java
@@ -0,0 +1,13 @@
+package com.capstonedesign.inhalife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InhaLifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InhaLifeApplication.class, args);
+	}
+
+}
