b0e747d3e20644bb36678cada9806f7660082728
sp.arc <pcpark98@naver.com>
2023-10-31 16:27:06 +0900

    feat: 에러 처리 작업

---
+++ src/main/java/com/capstonedesign/inhalife/exception/domain/ServiceException.java
@@ -0,0 +1,13 @@
+package com.capstonedesign.inhalife.exception.domain;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public class ServiceException extends RuntimeException {
+
+    final HttpStatus httpStatus;
+    final String message;
+}
