83a9ee38f0849dc9fc02fdee8e82c9e445805a79
sp.arc <pcpark98@naver.com>
2023-10-31 17:07:18 +0900

    feat: SubjectTaken 도메인

---
+++ src/main/java/com/capstonedesign/inhalife/subject/domain/SubjectTaken.java
@@ -0,0 +1,48 @@
+package com.capstonedesign.inhalife.subject.domain;
+
+import com.capstonedesign.inhalife.user.domain.User;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+import java.time.LocalDateTime;
+
+import static javax.persistence.FetchType.*;
+
+@Entity
+@Getter @Setter
+@Table(
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        columnNames = {"user_index", "subject_index"}
+                )
+        }
+)
+public class SubjectTaken {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "subject_taken_index")
+    private Long id;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "user_index")
+    private User user;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "subject_index")
+    private Subject subject;
+
+    @NotNull
+    @Column(name = "school_year")
+    private Integer schoolYear;
+
+    @NotNull
+    private Boolean semester;
+
+    @CreationTimestamp
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+}
