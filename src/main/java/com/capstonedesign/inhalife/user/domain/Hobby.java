1dbcdb6a1351186743307c3f1bcbcc7d2e7b7590
sp.arc <pcpark98@naver.com>
2023-11-01 17:04:46 +0900

    feat: Hobby 도메인

---
+++ src/main/java/com/capstonedesign/inhalife/user/domain/Hobby.java
@@ -0,0 +1,22 @@
+package com.capstonedesign.inhalife.user.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
+@Getter @Setter
+public class Hobby {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "hobby_index")
+    private Long id;
+
+    @NotBlank
+    @Column(length = 30)
+    @Size(max = 30)
+    private String name;
+}
