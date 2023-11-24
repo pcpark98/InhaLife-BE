69788369b91cb3de756da96ae909bab477c3f731
sp.arc <pcpark98@naver.com>
2023-11-25 02:35:12 +0900

    feat: 대회 관련 도메인

---
+++ src/main/java/com/capstonedesign/inhalife/contest/domain/Field.java
@@ -0,0 +1,27 @@
+package com.capstonedesign.inhalife.contest.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+public class Field {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "field_index")
+    private Long id;
+
+    @NotBlank
+    @Column(length = 30)
+    @Size(max = 30)
+    private String name;
+
+    @OneToMany(mappedBy = "field")
+    private List<FieldOfContest> fieldOfContestList = new ArrayList<>();
+}
