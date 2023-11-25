a2cf4ad4e45711ae05fe763b5935b3ff6f9a7391
sp.arc <pcpark98@naver.com>
2023-11-25 23:43:27 +0900

    feat: 취미 도메인 수정

--- src/main/java/com/capstonedesign/inhalife/user/domain/Hobby.java
+++ src/main/java/com/capstonedesign/inhalife/user/domain/Hobby.java
@@ -6,6 +6,7 @@ import lombok.Setter;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.util.ArrayList;
 import java.util.List;
@@ -23,6 +24,9 @@ public class Hobby {
     @Column(length = 30)
     @Size(max = 30)
     private String name;
+
+    @NotNull
+    private int score;
 
     @OneToMany(mappedBy = "hobby")
     private List<HobbyOfUser> hobbyOfUserList = new ArrayList<>();
